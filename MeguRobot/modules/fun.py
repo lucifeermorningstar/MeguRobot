fdbc864426003a189ef16aae2ec0b405b226e118
NachABR <agusabrigo12@gmail.com>
2021-03-28 18:36:35 -0300

    fun

--- MeguRobot/modules/fun.py
+++ MeguRobot/modules/fun.py
@@ -464,7 +464,7 @@ def slap(update: Update, context: CallbackContext):
     user_id = extract_user(message, args)
 
     if user_id == bot.id:
-        temp = random.choice(slap_megu_templates)
+        temp = random.choice(SLAP_MEGU_TEMPLATES)
 
         if isinstance(temp, list):
             if temp[2] == "tmute":
