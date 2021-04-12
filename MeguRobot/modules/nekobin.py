4b752f3221ac329b8948196c59994859f8480c82
NachABR <agusabrigo12@gmail.com>
2021-04-12 17:53:17 -0300

    nekobin

--- MeguRobot/modules/nekobin.py
+++ MeguRobot/modules/nekobin.py
@@ -10,56 +10,40 @@ from nekobin import NekoBin
 
 async def paste(client, message):
     nekobin = NekoBin()
-    if not message.reply_to_message or not message.reply_to_message.document:
-        await message.reply_text("Dame algo para copiar!")
-    elif message.reply_to_message:
-        text = message.reply_to_message.text
-    elif (
-        message.reply_to_message.document
-        and message.reply_to_message.document.file_size < 2 ** 20 * 10
-    ):
-        var = os.path.splitext(message.reply_to_message.document.file_name)[1]
-        print(var)
-        path = await message.reply_to_message.download("temp/")
-        with open(path) as doc:
-            text = doc.read()
-        os.remove(path)
-    if text:
+    try:
+        if (
+            message.reply_to_message.document
+            and message.reply_to_message.document.file_size < 2 ** 20 * 10
+        ):
+            var = os.path.splitext(message.reply_to_message.document.file_name)[1]
+            print(var)
+            path = await message.reply_to_message.download("temp/")
+            with open(path) as doc:
+                text = doc.read()
+            os.remove(path)
+        if message.reply_to_message:
+            text = message.reply_to_message.text
         try:
             response = await nekobin.nekofy(text)
-        except:
+        except Exception:
             await message.reply_text("Ocurrió un error al copiar...")
-            return
-    else:
+    except:
+        await message.reply_text("Dame algo para copiar!")
+    try:
         text = "Copiado a **Nekobin**:\n"
         text += f" • [Link]({response.url})"
         text += f" | [Raw]({response.raw})"
-        delete = bool(
-            len(message.command) > 1
-            and message.command[1] in ["d", "del"]
-            and message.reply_to_message.from_user.is_self,
+        await message.reply_text(
+            text, disable_web_page_preview=True, parse_mode="md"
         )
-        if delete:
-            await asyncio.gather(
-                client.send_message(
-                    message.chat.id,
-                    text,
-                    disable_web_page_preview=True,
-                    parse_mode="md",
-                )
-            )
-        else:
-            await message.reply_text(
-                text, disable_web_page_preview=True, parse_mode="md"
-            )
+    except:
+        pass
 
 
 async def get_paste_(client, message):
     """fetches the content of a dogbin or nekobin URL."""
-    link = message.reply_to_message.text
-    if not link:
-        await client.send_message(message.chat.id, text="Dame un link!")
-        return
+    if message.reply_to_message:
+        link = message.reply_to_message.text
     rep = await client.send_message(
         message.chat.id, text="Obteniendo el contenido copiado..."
     )
@@ -80,7 +64,7 @@ async def get_paste_(client, message):
         link = link[len("nekobin.com/") :]
         raw_link = f"https://nekobin.com/raw/{link}"
     else:
-        await rep.edit_text(text="Dame un link!")
+        await rep.edit_text(text="Responde a un link de **NekoBin** o **DogBin**!")
         return
     resp = await AioHttp().get_text(raw_link)
     await client.send_message(message.chat.id, text=f"**Contenido:**\n`{resp}`")
